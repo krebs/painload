4a81dc231198467ebc6948479ef34ba05b964811
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-02 02:14:59 +0200

    updated debian installer for autostart,no.de installer(nonfunct)

--- build_debian.sh
+++ build_debian.sh
@@ -4,8 +4,13 @@ MYIP=10.0.7.7.55
 
 aptitude install tinc git
 
+
 git clone https://github.com/makefu/shack-retiolum.git
 
 cd shack-retiolum
 
 ./install.sh `hostname` $MYIP
+
+# for autostart
+echo "retiolum" >> /etc/tinc/nets.boot
+echo "EXTRA=\"--user=tincd --chroot\"" >> /etc/default/tinc
