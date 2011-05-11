513b1ad5c490f9f3821fc6aa2a9bddf3c3f31802
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-11 13:17:21 +0200

    refactored parser script,fixed bug
    parse.py: refactored parsing into function
    sanitize.sh: fixed bug which creates race condition, removed gvcolor from
      toolchain

--- retiolum/hosts/.scripts/sanitize.sh
+++ retiolum/hosts/.scripts/sanitize.sh
@@ -1,2 +1,2 @@
-sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[0-9]*\]: //gp}' /var/log/everything.log | ./parse.py | tee here.dot | dot | gvcolor | dot -Tpng -O
-mirage noname.dot.png
+sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[1-9]*\]: //gp}' /var/log/everything.log |  ./parse.py | tee here.dot | dot -Tpng -o retiolum.png
+mirage retiolum.png
