b719d0e21d6f0fb8476fbd8acc50f0a01ebbf804
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-11 13:48:07 +0200

    refactored parse.py
    parse.py: now also writes the number of current connections

--- retiolum/hosts/.scripts/sanitize.sh
+++ retiolum/hosts/.scripts/sanitize.sh
@@ -1,2 +1,2 @@
-sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[1-9]*\]: //gp}' /var/log/everything.log |  ./parse.py | tee here.dot | dot -Tpng -o retiolum.png
+sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[1-9]*\]: //gp}' /var/log/everything.log |  ./parse.py | tee retiolum.dot | dot -Tpng -o retiolum.png
 mirage retiolum.png
