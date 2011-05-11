eaf462c44f518227091a4c2b616d2ba122288d23
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-11 12:38:40 +0200

    added tinc syslog parser and graph generator
    parse.py: parses syslog input given via stdin, writes graphviz graph to
      stdout
    sanitize.sh:helper script which prepares syslog for the parse script,pipes
      output of parse.sh into graphviz

---
+++ hosts/.scripts/parse.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python2
+
+import sys
+
+def write_digraph(nodes):
+  print ('digraph retiolum {')
+  print ('  node[shape=box,style=filled,fillcolor=grey]')
+  for k,v in nodes.iteritems():
+    write_node(k,v)
+  print ('}')
+def write_node(k,v):
+  node = "  "+k+"[label=\""
+  node += k+"\\l"
+  node += "external:"+v['external-ip']+":"+v['external-port']+"\\l"
+  node += "internal:"+v['internal-ip']+"\\l\""
+  if v['external-ip'] == "MYSELF":
+    sys.stderr.write("lolwut")
+    node += ",fillcolor=steelblue1"
+  node += "]"
+  print (node)
+  for con in v.get('to',[]):
+    print "  "+k+ "->" +con['name'] + "[weight="+str(10/float(con['weight']))+"]"
+nodes={}
+edges={}
+
+for line in sys.stdin:
+  line = line.replace('\n','')
+  if line == 'Nodes:':
+    nodes={}
+    for line in sys.stdin:
+      if line == 'End of nodes.\n':
+        break
+      l = line.replace('\n','').split() #TODO unhack me
+      nodes[l[0]]= { 'external-ip': l[2], 'external-port' : l[4] }
+  if line == 'Subnet list:':
+    for line in sys.stdin:
+      if line == 'End of subnet list.\n':
+        break
+      l = line.replace('\n','').split() 
+      nodes[l[2]]['internal-ip'] = l[0].split('#')[0]
+  if line == 'Edges:':
+    edges = {}
+    for line in sys.stdin:
+      if line == 'End of edges.\n':
+        break
+      l = line.replace('\n','').split() 
+
+      if not nodes[l[0]].has_key('to') :
+        nodes[l[0]]['to'] = []
+      nodes[l[0]]['to'].append(
+          {'name':l[2],'addr':l[4],'port':l[6],'weight' : l[10] })
+
+write_digraph(nodes)
