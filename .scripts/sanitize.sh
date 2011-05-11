bd6fced9c61ed0b343d3b011afea22f12dfcedb2
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-11 19:14:21 +0200

    fixed race condition error

--- .scripts/sanitize.sh
+++ .scripts/sanitize.sh
@@ -4,7 +4,7 @@ LOG_FILE=/var/log/everything.log
 OPENER=/bin/true
 
 sudo pkill -USR2 tincd
-sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[1-9]*\]: //gp}' $LOG_FILE |\
+sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[0-9]*\]: //gp}' $LOG_FILE |\
     ./parse.py > retiolum.dot
 
 $GRAPH_SETTER1 -Tpng -o retiolum_1.png retiolum.dot
