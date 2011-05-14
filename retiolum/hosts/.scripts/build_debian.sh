6514dfb5482368123b7a61b7fa778cdc71283661
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-05-14 22:45:58 +0200

    updated debian build script to behave correctly in the current context

--- retiolum/hosts/.scripts/build_debian.sh
+++ retiolum/hosts/.scripts/build_debian.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -x
-MYIP=10.0.7.7.55
+if [ ! "$MYIP" ] 
+then
+  MYIP=10.0.7.7.55
+fi
+if [ ! "$MYHOSTNAME" ]
+then
+  MYHOSTNAME="penis"
+fi
 
+if [ "$MYHOSTNAME" = "penis" ];
+then 
+  read -n1 -p "name is penis, are u sure? [yN]" 
+  if [[ "$REPLY" != [yY] ]] 
+  then 
+    echo "then better RTFC"
+    echo "bailing out"  
+    exit 0
+  fi
+fi
 apt-get install tinc git curl python
 
-git clone https://github.com/makefu/shack-retiolum.git
+./install.sh "$MYHOSTNAME" "$MYIP"
 
-cd shack-retiolum
-
-./install.sh `hostname` $MYIP
-cd ..
-rm shack-retiolum
 # for autostart
 sed -i '/retiolum/d' /etc/tinc/nets.boot
 echo "retiolum" >> /etc/tinc/nets.boot
