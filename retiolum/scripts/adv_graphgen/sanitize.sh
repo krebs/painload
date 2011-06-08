e89ede8ec7e8893c6fe23f456fbc4de4ab04804c
Felix Richter <Felix.Richter@syntax-fehler.de>
2011-06-08 18:36:33 +0200

    adv_graphgen/sanitize.sh: added sfdp graph setter

--- retiolum/scripts/adv_graphgen/sanitize.sh
+++ retiolum/scripts/adv_graphgen/sanitize.sh
@@ -1,6 +1,7 @@
 GRAPH_SETTER1=dot
 GRAPH_SETTER2=circo
 GRAPH_SETTER3=neato
+GRAPH_SETTER3=sfdp
 LOG_FILE=/var/log/everything.log
 OPENER=/bin/true
 
@@ -11,5 +12,6 @@ sudo sed -n '/tinc.retiolum/{s/.*tinc.retiolum\[[0-9]*\]: //gp}' $LOG_FILE |\
 $GRAPH_SETTER1 -Tpng -o $1retiolum_1.png retiolum.dot
 $GRAPH_SETTER2 -Tpng -o $1retiolum_2.png retiolum.dot
 $GRAPH_SETTER3 -Tpng -o $1retiolum_3.png retiolum.dot
+$GRAPH_SETTER4 -Tpng -o $1retiolum_4.png retiolum.dot
 $OPENER retiolum_1.png &>/dev/null 
 #rm retiolum.dot
