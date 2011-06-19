fb6410930c67938ec32aed2b9c5ec3e26a66869a
tv <tv@also>
2011-06-20 00:52:46 +0200

    cobra needs no disarm

--- cobra/index.sh
+++ cobra/index.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 set -euf
 trap "echo 'You are made of stupid!' >&2; exit 23" EXIT
-disarm() {
-  trap - EXIT
-}
 
 COBRA_PATH="${COBRA_PATH-$PWD}"
 
@@ -13,7 +10,6 @@ for target; do
     if test -d "$path/$target"; then
       if index="$path/$target/index.sh" && test -f "$index"; then
         exec /bin/sh "$index"
-        disarm
       fi
     fi
   done
