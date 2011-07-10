aa57527badc83b49a73ac7708976d88079ea8c23
Chinaman <root@chinaman>
2011-07-10 06:23:22 +0200

    updated cruise control, now incompatible with linucCoin!
    mining/bin/newkey.sh: unused because slime is now in place
    mining/user/startup.sh: edited paths to miner, NOW INCOMPATIBLE with linucCoin (sucked anyway)
    temper/Makefile: fixed issue when installing temper (lose suid bit)

--- mining/user/startup.sh
+++ mining/user/startup.sh
@@ -1,16 +1,13 @@
-sudo pkill xscreensaver
 sudo pkill tmux
-sleep 1
+sleep 5
+source ~/.profile
 export DISPLAY=:0
-
 export URL="${URL-http://txgen_chinaman:lolwut@uk.btcguild.com:8332}"
 #export URL="http://txgen:qJrXefWX@bitcoinpool.com:8334"
-slock &
 tmux start-server
 tmux new-session -d -s mining -n mining
-printenv > /home/user/environment
-tmux new-window -t mining:1 'AMDOverdriveCtrl -i 0  mining.ovdr' 
-tmux new-window -t mining:2 'AMDOverdriveCtrl -i 3  mining.ovdr' 
+tmux new-window -t mining:1 'cd ~;AMDOverdriveCtrl -i 0 mining.ovdr' 
+tmux new-window -t mining:2 'cd ~;AMDOverdriveCtrl -i 3 mining.ovdr' 
 sleep 5
-tmux new-window -t mining:3 "cd /opt/miners/phoenix; while sleep 1; do sudo python phoenix.py -u $URL -k phatk DEVICE=0 VECTORS BFI_INT WORKSIZE=256 AGGRESSION=12 FASTLOOPS=false;done"
-tmux new-window -t mining:4 "cd /opt/miners/phoenix; while sleep 1;do  sudo python phoenix.py -u $URL -k phatk DEVICE=1 VECTORS BFI_INT WORKSIZE=256 AGGRESSION=10 FASTLOOPS=false ; done"
+tmux new-window -t mining:3 "cd /usr/src/phoenix-miner/; while sleep 1; do sudo ./phoenix.py -u $URL -k phatk DEVICE=0 VECTORS BFI_INT WORKSIZE=256 AGGRESSION=12 FASTLOOPS=false;done"
+tmux new-window -t mining:4 "cd /usr/src/phoenix-miner/; while sleep 1; do sudo ./phoenix.py -u $URL -k phatk DEVICE=1 VECTORS BFI_INT WORKSIZE=256 AGGRESSION=12 FASTLOOPS=false;done"
