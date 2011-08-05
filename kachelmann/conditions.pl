dbefb844577d85a48675e598ef04c4590661952f
root <root@krebs>
2011-08-06 00:25:26 +0200

    Added Kachelmann for weathermonitoring

---
+++ kachelmann/conditions.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use XML::Simple;
+system("w3m -dump \"http://www.google.com/ig/api?weather=70327-stuttgart&hl=de\" > weather.xml");
+my $xml = new XML::Simple;
+my $data = $xml->XMLin("weather.xml");
+my $sentence = "Die Wetterkondition ist: $data->{weather}->{current_conditions}->{condition}->{data}\n bei $data->{weather}->{current_conditions}->{temp_c}->{data} Grad Celsius\n";
+system("espeak \"$sentence\"");
