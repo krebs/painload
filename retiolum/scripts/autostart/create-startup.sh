2048448601aa27f993268ef67bf0eeda3625ef4e
tv <tv@xso>
2011-08-12 23:23:33 +0200

    retiolum: go more enterprise

--- retiolum/scripts/autostart/create-startup.sh
+++ retiolum/scripts/autostart/create-startup.sh
@@ -6,12 +6,17 @@ if test "${nosudo-false}" != true -a `id -u` != 0; then
   exit 23 # go to hell
 fi
 
+readlink="`readlink -f "$0"`"
+dirname="`dirname "$0"`"
+cd "$dirname"
+
 if [ -e /etc/init.d ];then
   INIT_FOLDER=/etc/init.d 
 	update-rc.d tinc defaults #TODO debian specific
 else
+  INIT_FOLDER=/etc/rc.d
 	echo "add tinc to DAEMONS in /etc/rc.conf" #TODO archlinux specific
 fi
 
 echo "retiolum" > /etc/tinc/nets.boot
-cp -a tinc $INITFOLDER
+cp -a tinc $INIT_FOLDER
