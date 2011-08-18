ff4808b6b9a7ab1dc6711c01e3c385eda78a241d
Momo <momorientes@online.de>
2011-08-18 04:17:17 +0200

    Fixed the READMES and disconnect_narf.pl

--- filebitch/disconnect_narf.pl
+++ filebitch/disconnect_narf.pl
@@ -1,15 +1,16 @@
 #!/usr/bin/perl
 #Please add the following to your proftpd config file 
-#ExtendedLog     /var/log/proftpd/ftp_auth.log AUTH auth
-#and
 #<IfModule mod_exec.c>
 #    ExecEngine on
-#    ExecOnExit "/krebs/filebitch/disconnect_narf.pl"
+#    ExecOnExit "/krebs/filebitch/connect_narf.pli %a"
 #</IfModule>
 
-#$ip = system("tail -n 1 /var/log/proftpd/ftp_auth.log");
+$ip = $ARGV[0];
 #I'm very sorry for this regex, but i only wanted it to get _real_ IPv4 Adresses of the log file, not any kind of timestamp bullshit
-#$ip =~ s/\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b//g;
+$ip =~ s/\b(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\b//g;
 #getting some guys sitting next to the Server pissed :)
-my $ip = "USER";
-system("beep -l 42 -f 2000 -D 42 -n -l 42 -f 1000");
+system("morse -l 42 -f 2000 $ip");
+system("morse -l 42 -f 3000 \"disconnected\"");
+
+# Uncomment the beep below to play the enterprise connect sound
+# system("beep -l 42 -f 2000 -D 42 -n -l 42 -f 3337");
