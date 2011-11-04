fc5b5f2562cbfd9028f6b77269d123261dae89e9
EUcancER <root@euer.krebsco.de>
2011-11-04 13:41:01 +0100

    //retiolum/adv_graphgen: bug-fixing
    debian uses features of adv_graphgen which hasn't been tested until now. Bugs has been fixed concerning usage of LOG_FILE environ
    and many more
    
    fix minor calculation bug in availability calucluation (delete faulty archive lines)

--- retiolum/scripts/adv_graphgen/sanitize.sh
+++ retiolum/scripts/adv_graphgen/sanitize.sh
@@ -4,14 +4,13 @@ GRAPH_SETTER1=dot
 GRAPH_SETTER2=circo
 GRAPH_SETTER3='neato -Goverlap=prism '
 GRAPH_SETTER4=sfdp
-#LOG_FILE=/var/log/syslog
+LOG_FILE=/var/log/syslog
 TYPE=svg
 TYPE2=png
 OPENER=/bin/true
 DOTFILE=`mktemp`
-trap 'rm $DOTFILE' SIGINT SIGTERM
-sudo pkill -USR2 tincd
-sudo python tinc_stats.py |\
+trap 'rm $DOTFILE' SIGTERM
+sudo LOG_FILE=$LOG_FILE python tinc_stats.py |\
     python parse_tinc_stats.py > $DOTFILE
 
 $GRAPH_SETTER1 -T$TYPE -o $1/retiolum_1.$TYPE $DOTFILE
