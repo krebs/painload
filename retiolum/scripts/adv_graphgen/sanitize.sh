91d6fc8971483bf8e1fed7d649f966d180e23956
EUcancER <root@euer.krebsco.de>
2012-01-04 01:32:53 +0100

    //retiolum/adv_graphgen/sanitize: be less verbose

--- retiolum/scripts/adv_graphgen/sanitize.sh
+++ retiolum/scripts/adv_graphgen/sanitize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -xeuf
+set -euf
 cd $(dirname `readlink -f $0`)
 GRAPH_SETTER1=dot
 GRAPH_SETTER2=circo
@@ -21,10 +21,12 @@ do
   tmpgraph=`mktemp --tmpdir=$1`
   $setter -T$TYPE -o $tmpgraph $DOTFILE
   chmod go+rx $tmpgraph
-  mv $tmpgraph $1retiolum_$i.$TYPE
+  mv $tmpgraph $1/retiolum_$i.$TYPE
   i=`expr $i + 1`
 done
+
 #convert -resize 20% $1/retiolum_1.$TYPE  $1/retiolum_1.$TYPE2
 #convert -resize 20% $1/retiolum_2.$TYPE  $1/retiolum_2.$TYPE2
 #convert -resize 20% $1/retiolum_3.$TYPE  $1/retiolum_3.$TYPE2
 #convert -resize 20% $1/retiolum_4.$TYPE  $1/retiolum_4.$TYPE2
+rm $DOTFILE
