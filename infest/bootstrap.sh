fe7570c86536b6ca84aad115bc516b5e4f9c3990
makefu <github@syntax-fehler.de>
2012-03-10 01:01:22 +0100

    refactor bootstrap file

--- infest/bootstrap.sh
+++ infest/bootstrap.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
-set -x
+set -euf
+# Can be overwritten before install
+KREBSDIR=${KREBSDIR:-/krebs}
 [ "`id -u`" -eq "0" ] || { echo "you need to be root!"; exit 1;} || exit 1
 
-[ -e '/usr/bin/git' ] || \
-apt-get install -y git-core || \
-yum install git || \
-opkg install git || \
-pacman -Sy git || \
-{ echo "please install git!"; exit 1;} || exit 1
+command -v git || \
+    apt-get install -y git-core || \
+    yum install git || \
+    opkg install git || \
+    ipkg install git || \
+    pacman -Sy git || \
+    { echo "please install git manually!"; exit 1;} || exit 1
 
-[ -e '/krebs' ] || git clone git://github.com/krebscode/painload.git /krebs \
-|| { echo "cloning failed :(" ; exit 1; } || exit 1
+[ -e "$KREBSDIR" ] || git clone https://github.com/krebscode/painload.git "$KREBSDIR" \
+    || { echo "cloning failed :(" ; exit 1; } || exit 1
 
-cd /krebs || { echo "cannot change into /krebs folder:(" ; exit 1; } || exit 1
+cd $KREBSDIR || { echo "cannot change into $KREBSDIR folder:(" ; exit 1; } || exit 1
 
-#read -n1 -p "infest now? [yN]"
-
-#[[ $REPLY = [yY] ]] && make infest 
-#echo $REPLY
-echo "do 'make infest' in /krebs"
+echo "do 'make infest' in $KREBSDIR"
 echo "have a nice day"
 
+
+PATH=$PATH:$KREBSDIR/punani/bin
+
+# bootstrap the rest
+punani install make
