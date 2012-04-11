66a89c3311e126b0280369700ee6549683cfee99
momo <m@hackathon.de>
2012-04-11 13:40:33 +0200

    //util/bin/naturalvoices2.sh: adding variables easy configurability

--- util/bin/naturalvoices2.sh
+++ util/bin/naturalvoices2.sh
@@ -1,3 +1,6 @@
 text=$(echo $* | sed 's/ /+/g')
-mplayer http://192.20.225.36$( curl -Ss -A "Mozilla" -d "voice=klara" -d "txt=$text" -d "speakButton=SPEAK" http://192.20.225.36/tts/cgi-bin/nph-nvdemo |grep HREF|sed 's/.*\(".*"\).*/\1/' |sed -e 's/"//g')
+voice="klara"
+base_url="http://192.20.225.36"
+
+mplayer $base_url$( curl -Ss -A "Mozilla" -d "voice=$voice" -d "txt=$text" -d "speakButton=SPEAK" $base_url/tts/cgi-bin/nph-nvdemo |grep HREF|sed 's/.*\(".*"\).*/\1/' |sed -e 's/"//g')
 
