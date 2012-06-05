c04b0047db2249317a905f7d95d2fcaef6a479cc
EUcancER <root@euer.krebsco.de>
2012-06-05 12:05:13 +0200

    //retiolum all_the_graphs: update to be non-intrusive

--- retiolum/scripts/adv_graphgen/all_the_graphs.sh
+++ retiolum/scripts/adv_graphgen/all_the_graphs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-echo "`date` begin all graphs" >> /tmp/build_graph
-cd $(dirname $(readlink -f $0))
-PATH=$PATH:../../../util/bin/
-export LOG_FILE=/var/log/retiolum.log
-begin=`timer`
-(./anonytize.sh /srv/http/pub/graphs/retiolum/ && echo "`date` anonytize done" >> /tmp/build_graph)&
-(./sanitize.sh /srv/http/priv/graphs/retiolum/ && echo "`date` sanitize done" >> /tmp/build_graph)&
-for job in `jobs -p`
-do
-  echo $job
-  wait $job || echo "$job failed!"
-done
-graphitec "retiolum.graph.buildtime" "$(timer $begin)"
+set -x
+
+(
+  1>>/tmp/build_graph
+  echo "`date` begin all graphs" >> /tmp/build_graph
+  cd $(dirname $(readlink -f $0))
+  PATH=$PATH:../../../util/bin/
+  export LOG_FILE=/var/log/retiolum.log
+  begin=`timer`
+  (./anonytize.sh /srv/http/pub/graphs/retiolum/ && echo "`date` anonytize done" >> /tmp/build_graph)&
+  (./sanitize.sh /srv/http/priv/graphs/retiolum/ && echo "`date` sanitize done" >> /tmp/build_graph)&
+  wait
+  graphitec "retiolum.graph.buildtime" "$(timer $begin)"  &>>/tmp/build_graph
+)&
