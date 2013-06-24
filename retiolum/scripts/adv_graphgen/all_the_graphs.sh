8ac465a57fb13e39bfc2593b467b33fc99173803
makefu <root@pigstarter.de>
2013-06-24 13:18:46 +0200

    update all the graphs

--- retiolum/scripts/adv_graphgen/all_the_graphs.sh
+++ retiolum/scripts/adv_graphgen/all_the_graphs.sh
@@ -4,12 +4,12 @@
   echo "`date` begin all graphs" >> /tmp/build_graph
   cd $(dirname $(readlink -f $0))
   PATH=$PATH:../../../util/bin/
-  export LOG_FILE=/var/log/syslog
+  export LOG_FILE=/var/log/tinc-retiolum.log
   export TINC_LEGACY=true
   EXTERNAL_FOLDER=/var/www/euer.krebsco.de/graphs/retiolum
   INTERNAL_FOLDER=/var/www/euer/graphs/retiolum
   begin=`timer`
-  export GRAPHITE_HOST="no_omo"
+  export GRAPHITE_HOST="no-omo"
   (./anonytize.sh $EXTERNAL_FOLDER && echo "`date` anonytize done" >> /tmp/build_graph)&
   (./sanitize.sh $INTERNAL_FOLDER && echo "`date` sanitize done" >> /tmp/build_graph)&
 #  wait
