d93cc223bba4d1f5a4100ac0967e442ba051e6da
makefu <github@syntax-fehler.de>
2013-07-30 22:20:25 +0200

    natural voices is using aplay instead of mplayer

--- util/bin/naturalvoices2.sh
+++ util/bin/naturalvoices2.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
 text=$(echo $* | sed 's/ /+/g')
 voice="klara"
 base_url="http://192.20.225.36"
 
-mplayer $base_url$( curl -Ss -A "Mozilla" -d "voice=$voice" -d "txt=$text" -d "speakButton=SPEAK" $base_url/tts/cgi-bin/nph-nvdemo |grep HREF|sed 's/.*\(".*"\).*/\1/' |sed -e 's/"//g')
+TMPFILE=`mktemp`
+trap "rm $TMPFILE" TERM INT EXIT
 
+wget $base_url$( curl -Ss -A "Mozilla" -d "voice=$voice" -d "txt=$text" -d "speakButton=SPEAK" $base_url/tts/cgi-bin/nph-nvdemo |grep HREF|sed 's/.*\(".*"\).*/\1/' |sed -e 's/"//g') -O $TMPFILE
+aplay $TMPFILE
+rm $TMPFILE
