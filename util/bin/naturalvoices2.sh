d0c944a569be681433231760b053bc2fc5ed02e8
makefu <github@syntax-fehler.de>
2013-07-30 22:30:50 +0200

    fix trap

--- util/bin/naturalvoices2.sh
+++ util/bin/naturalvoices2.sh
@@ -8,4 +8,3 @@ trap "rm $TMPFILE" TERM INT EXIT
 
 wget $base_url$( curl -Ss -A "Mozilla" -d "voice=$voice" -d "txt=$text" -d "speakButton=SPEAK" $base_url/tts/cgi-bin/nph-nvdemo |grep HREF|sed 's/.*\(".*"\).*/\1/' |sed -e 's/"//g') -O $TMPFILE
 aplay $TMPFILE
-rm $TMPFILE
