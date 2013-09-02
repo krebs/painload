b8d58dfa8ba5ae2fae9ac755e0c663c5ba08576f
makefu <github@syntax-fehler.de>
2013-09-02 18:24:27 +0200

    //lib/naturalvoices/att.sh ' -> "

--- util/lib/naturalvoices/att.sh
+++ util/lib/naturalvoices/att.sh
@@ -16,11 +16,11 @@ get_tts(){
     voice="${voice:-klara}"
     ip=192.20.225.36
     base_url="http://$ip"
-    wget $base_url$( curl  -Ss  -H 'Host:$ip' \
+    wget $base_url$( curl  -Ss  -H "Host:$ip" \
         -H 'Origin:http://www2.research.att.com' \
-        -e "http://www2.research.att.com/~ttsweb/tts/demo.php" \
+        -e 'http://www2.research.att.com/~ttsweb/tts/demo.php' \
         -A "Mozilla/5.0 (X11; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0" \
-        -d "voice=$voice" -d "txt=$text" -d "speakButton=SPEAK" \
+        -d "voice=$voice" -d "txt=$text" -d 'speakButton=SPEAK' \
         -H 'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
             $base_url/tts/cgi-bin/nph-nvttsdemo | \
             grep HREF|sed 's/.*\(".*"\).*/\1/'  | \
