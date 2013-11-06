2f0f23ec0e54aaa0dc7a9c28217f7daca72ea455
lassulus <lassulus@googlemail.com>
2013-11-06 21:53:51 +0100

    webchat: nick and time display

--- webchat/hello_web.js
+++ webchat/hello_web.js
@@ -94,8 +94,8 @@ var app = connect()
     res.write('<script src="jquery-2.0.3.min.js"></script>');
     res.write('<script src="client.js"></script>');
     res.write('<div id=bg><div id=chatter>');
-    res.write('hello, this is #krebs:<br>');
-    res.write('<table id="chatbox"><tr id="foot"><td></td><td></td><td><input type="text" id="input"></td></tr></table>');
+    res.write('hello, this is the official krebs support:<br>');
+    res.write('<table id="chatbox"><tr id="foot"><td id="time"></td><td id="nick" class="chat_from"></td><td><input type="text" id="input"></td></tr></table>');
     res.end('</div></div>');
 
   })
