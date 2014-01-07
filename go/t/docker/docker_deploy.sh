087e20660883351d71b5cf0308ac853e04cbdf6d
root <root@cband>
2014-01-07 13:31:07 -0600

    go:t/docker fix CR in regex

--- go/t/docker/docker_deploy.sh
+++ go/t/docker/docker_deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd $(dirname $(readlink -f $0))
-set -euf
+set -xeuf
 rnd_port=$(shuf -i 2000-65000 -n 1)
 docker_id=$(docker run -p $rnd_port:80 -d -v  /krebs/go/t/docker/../../../:/krebs ubuntu /bin/bash /krebs/go/t/docker/dockertest/deploy)
 #docker run -p $rnd_port:80  -v  /krebs/go/t/docker/../../../:/krebs ubuntu /bin/bash /krebs/go/t/docker/dockertest/deploy
 echo $docker_id on $rnd_port
 trap "docker stop $docker_id;docker rm $docker_id" INT TERM EXIT QUIT
 i=0
-max_wait=20
+max_wait=30
 echo "waiting for install (takes about 3 minutes)"
 sleep 240
 while ! curl -s localhost:$rnd_port >/dev/null ;do
@@ -15,6 +15,6 @@ while ! curl -s localhost:$rnd_port >/dev/null ;do
     test $i -gt $max_wait && echo "timeout for installation reached, bailing out" && exit 1
     echo "http port not yet reachable ($i of $max_wait). waiting"
     sleep 10
-done
-short_uri=$(curl -F "uri=aids.balls" localhost:$rnd_port)
-curl $short_uri -v | grep location: | grep aids.balls
+done 
+short_uri=$(curl -F "uri=aids.balls" localhost:$rnd_port| sed 's/\r$//')
+curl $short_uri -v 2>&1 | grep location: | grep aids.balls
