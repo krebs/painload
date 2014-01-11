d68bb6c556fa7da246697d309a886daed4cae9c6
lassulus <lassulus@googlemail.com>
2014-01-11 13:27:30 +0100

    news: more sanity!

--- news/controller.py
+++ news/controller.py
@@ -55,7 +55,7 @@ class NewsBot(irc.bot.SingleServerIRCBot):
 
 class commands():
     def add(args): 
-        if args[1] not in bots:
+        if args[1] not in bots and not knews.name:
             bot = rssbot.RssBot(args[2], args[1], url_shortener=url_shortener)
             bots[args[1]] = bot
             bot.start()
