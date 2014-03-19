c2aeadc448295b58ca99d2fb638bbbe4cab55887
makefu <github@syntax-fehler.de>
2014-03-19 20:14:04 +0100

    add reaktor bonus commands

---
+++ filehooker/root-image/krebs/etc/Reaktor/config.py
@@ -0,0 +1,39 @@
+import socket
+
+debug = False
+
+name = socket.gethostname()
+
+irc_alarm_timeout = 300
+irc_hammer_interval = 10
+irc_kill_timeout = 360
+irc_nickname = name
+irc_server = 'irc.freenode.org'
+irc_port = 6667
+irc_channels = [
+  '#filehooker'
+]
+
+def default_command(cmd):
+  return {
+    'capname': cmd,
+    'pattern': '^(?:' + name + '|\\*):\\s*' + cmd + '\\s*(?:\\s+(?P<args>.*))?$',
+    'argv': [ 'commands/' + cmd ] }
+
+commands = [
+  default_command('caps'),
+  default_command('hello'),
+  default_command('reload'),
+  default_command('badcommand'),
+  default_command('rev'),
+  default_command('uptime'),
+  default_command('shares'),
+  default_command('onion'),
+  default_command('nocommand'),
+  # command not found
+  { 'pattern': '^(?:' + name + '|\\*):.*',
+    'argv': [ 'commands/respond','You are made of stupid!'] },
+  # "highlight"
+  { 'pattern': '.*\\b' + name + '\\b.*',
+    'argv': [ 'commands/say', 'I\'m famous' ] }
+]
