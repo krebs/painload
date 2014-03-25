345e40a9be080f77945aaf8cbd11865ca834a035
makefu <github@syntax-fehler.de>
2014-03-25 23:39:56 +0100

    fix add-reaktor-secret

--- filehooker/root-image/krebs/bin/add-reaktor-secret.sh
+++ filehooker/root-image/krebs/bin/add-reaktor-secret.sh
@@ -5,14 +5,13 @@ red='\e[0;31m'
 nc='\e[0m'
 black='\e[0;30m'
 
-
 printf "${green}Add a Reaktor Secret${nc}\n"
 
 printf "provide Nick Name:\n"
 nick=${1:-}
 test -z "${nick:-}" && read nick
 printf "provide Secret:$black\n" 
-nick=${2:-}
+secret=${2:-}
 test -z "${secret:-}" && read secret
-
-printf "${nc}nick secret"
+echo "$nick $secret" >> /krebs/painload/Reaktor/admin.lst
+printf "${green}done${nc}"
